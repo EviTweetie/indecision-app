33605f98da64ec9d28498496f3690107176ba0ab
EviTweetie <EviTweetie@gmail.com>
2018-03-28 23:58:52 +0200

    Abschnitt 3 Lektion 12 Version simple if statement

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 console.log("App.js is running!")
+/* 
+Commit: 
+Abschnitt 3 Lektion 12
+*/
+
+// content
+/* 
+    if statements
+    ternary operator
+    logical AND operator
+*/
 
 // JSX - JavaScript XML
 
@@ -19,6 +30,31 @@ var template = (
     </div>
 )
 
+//if statement with user
+var user = {
+    name: 'Peter',
+    age: 24,
+    location: 'Danmark'
+}
+
+//function to get user.location
+function getLocation(location) {
+    if (location) {
+        return {location}
+    } else {
+        return 'no location defined'
+    }
+}
+
+var templateUser = (
+    <div>
+        <h1>User Info</h1>
+        <h2>{user.name}</h2>
+        <p>{user.age}</p>
+        <p>{getLocation(user.location)}</p>
+    </div>
+)
+
 var appRoot = document.getElementById("app")
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateUser, appRoot)
