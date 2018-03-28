d8925dc88611319442d2d2f3b07cb275bf8972a4
EviTweetie <EviTweetie@gmail.com>
2018-03-28 23:33:04 +0200

    app.js version mit object ohne feste textelemente

--- src/app.js
+++ src/app.js
@@ -2,20 +2,9 @@ console.log("App.js is running!")
 
 // JSX - JavaScript XML
 
-var templateOriginal = (
-  <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-)
-
 var app = {
     title: 'Indecision App',
-    subtitle: 'This is some info!'
+    subtitle: 'Put your life in the hands of a computer.'
 }
 console.log(app)
 
@@ -23,41 +12,13 @@ var template = (
     <div>
         <h1>{app.title}</h1>
         <p>{app.subtitle}</p>
-    </div>
-)
-
-//User via variables
-var userName = 'Hans'
-var userAge = 45
-var userLocation = 'Hamburg'
-
-//user object
-var user = {
-    name : 'Mona Lisa',
-    age : 35,
-    location : 'Bremen'
-}
-console.log(user)
-
-var templateTwo = (
-    <div>
-        <h1>Evi Tweetie</h1>
-        <p>My age: 48</p>
-        <p>Location: Hamburg</p>
-        
-        <h2>User via variables</h2>
-        <p>Name: {userName}</p>
-        <p>Age: {userAge}</p>
-        <p>Location: {userLocation}</p>
-
-        <h2>User via Object</h2>
-        <p>Name: {user.name}</p>
-        <p>Name: {user.age}</p>
-        <p>Name: {user.location}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
     </div>
 )
 
 var appRoot = document.getElementById("app")
 
-ReactDOM.render(template, appRoot);
-//ReactDOM.render(templateTwo, appRoot)
+ReactDOM.render(template, appRoot)
