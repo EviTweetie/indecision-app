8f89bf496c9916ea1081a6dac3b65c54720596bb
EviTweetie <evitweetie@gmail.com>
2018-04-15 21:34:02 +0200

    Update es6-arrow-functions-2.js

--- src/playground/es6-arrow-functions-2.js
+++ src/playground/es6-arrow-functions-2.js
@@ -6,7 +6,7 @@
 
 const user = {
   name: "Evi",
-  cities: ["Hamburg", "Schwiederstorf", "Harburg"],
+  cities: ["Oslo", "Kiel", "Hamburg"],
   printCities: function() {
     console.log('### ' + this.name + ' ###');
     console.log(this.cities);
@@ -16,7 +16,7 @@ const user = {
     //workaround 1: const that = this
     const that = this;
     this.cities.forEach(function(city) {
-      console.log(that.name + " wohnte in " + city);
+      console.log(that.name + " lived in " + city);
     });
   }
 };
@@ -26,8 +26,8 @@ user.printCities();
 //ARROW functions no longer bind THEIR OWN this value - instead it uses the this value of the context it has been created in (its parent's THIS VALUE).
 
 const userArrow = {
-  name: "Bulli",
-  cities: ["Delmenhorst", "Hamburg"],
+  name: "John",
+  cities: ["London", "Manchester"],
   //ES6 Method definition
   //printCities: function () {    >    printCities() {
   printCities() {
@@ -38,19 +38,19 @@ const userArrow = {
     //   console.log(this.name + " wohnte in " + city);
     // });
     //SHORTHAND
-    this.cities.forEach(city => console.log(this.name + " wohnte in " + city));
+    this.cities.forEach(city => console.log(this.name + " lived in " + city));
   }
 };
 
 userArrow.printCities();
 
 const userArrowMap = {
-    name: 'Oli',
-    cities: ['Blankenese', 'Iserbrook', 'Sülldorf'],
+    name: 'Karen',
+    cities: ['Arhus', 'Klitmoeller', 'Aalborg'],
     printCities() {
       console.log('### ' + this.name + ' ###');
       console.log(this.cities);
-      return this.cities.map((city) => this.name + ' wohnte schon in ' + city)
+      return this.cities.map((city) => this.name + ' lived in ' + city)
     }
   };
   //map returns a new array
