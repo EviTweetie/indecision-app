5edc56a4a0f07dd8c30a84bf614c8a7bb4952d00
EviTweetie <EviTweetie@gmail.com>
2018-04-15 20:05:28 +0200

    4.24 challenge class with methods, call with arguments (pass data in)

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,5 +1,5 @@
 class Person {
-  constructor(name = 'Person default') {
+  constructor(name = 'Person default', age = 0) {
     //constructor()
     //you can pass arguments in the constructor function
     //console.log('func constructor is here')
@@ -9,6 +9,7 @@ class Person {
     //in ES6 you can use function defaults
     //constructor(name = 'set default value') {
     this.name = name
+    this.age = age
   }
   //next method
   getGreeting() {
@@ -17,12 +18,21 @@ class Person {
       //use template strings (interpolation)
       return `Hi ${this.name} (interpolation)!`
   }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old.`
+  }
 }
 
-const me = new Person("Evi Bulli");
+const me = new Person("Evi Bulli", 47);
 console.log(me);
 console.log(me.getGreeting())
+console.log(me.getDescription())
 
 const other = new Person();
 console.log(other);
 console.log(other.getGreeting())
+console.log(other.getDescription())
+
+//4.24 challenge: 
+//add age as argument with default 0
+//add method getDescription - Andrew Mead is 26 year(s) old.
