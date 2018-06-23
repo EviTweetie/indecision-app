4fee087a96fa59674634997dd43687fbfea487da
EviTweetie <EviTweetie@gmail.com>
2018-06-24 01:55:49 +0200

    6.58 solved trouble with transform-class-properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,22 @@ import ReactDOM from "react-dom";
 import IndecisionApp from './components/IndecisionApp'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
+
+// ************ old syntax ************
+class OldSyntax {
+  constructor(props) {
+    this.name = "Chubby"
+  }
+}
+
+const oldSyntax = new OldSyntax()
+
+console.log(oldSyntax)
+
+// ************ new syntax ************
+class NewSyntax {
+  name = "Checker"
+}
+
+const newSyntax = new NewSyntax()
+console.log(newSyntax)
