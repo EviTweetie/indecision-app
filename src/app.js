58516fdd6ed6f8e5f0ba7110e9275181baefb10b
EviTweetie <EviTweetie@gmail.com>
2018-06-23 13:38:35 +0200

    6.51 ES6 import/export (explanation)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
-console.log('app.js')
-console.log(__dirname)
-console.log('hi evi')
+//import './utils'
+
+import { square, add } from './utils'
+//{} not an object here, but import syntax!!!
+
+console.log('app.js is running')
+
+console.log(square(5))
+console.log(add(10003, 4))
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+console.log('utils is running')
+
+//named export directly in variable declaration
+export const square = (x) => x * x
+
+export const add = (a, b) => a + b
+
+//named exports
+//export { square, add }
+//{} not an object here!!!
+
+//default
+//export default square
