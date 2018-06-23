87aa837402b1f01c6d45ab05dc4f25c9696df6b9
EviTweetie <EviTweetie@gmail.com>
2018-06-23 22:03:10 +0200

    6.53 import modules

--- src/app.js
+++ src/app.js
@@ -1,18 +1,4 @@
-//import './utils'
 
-import { square, add } from './utils'
-//{} not an object here, but import syntax!!!
-import isSenior, { isAdult, canDrink } from './person'
+import validator from 'validator'
 
-console.log('app.js is running')
-
-console.log(square(5))
-console.log(add(10003, 4))
-console.log(isAdult(5))
-console.log(canDrink(34))
-
-const personAge = 15
-
-console.log(isAdult(personAge));
-console.log(canDrink(personAge));
-console.log(isSenior(50))
+console.log(validator.isEmail('evi@tweetie.com'))
