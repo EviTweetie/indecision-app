a5b8a6239d6c2ef1478589479f4d2c3468a622f1
EviTweetie <EviTweetie@gmail.com>
2018-06-23 23:24:44 +0200

    6.55 refactoring: one module per component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import IndecisionApp from './components/IndecisionApp'
 
-const template = <p>test - JSX from WEBPACK works</p>
-//error because no babel so far
-
-//use without babel
-//const template = React.createElement('p', {}, 'test')
-
-ReactDOM.render(template, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
---
+++ src/components/AddOption.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: undefined
+        };
+        this.handleAddOption = this.handleAddOption.bind(this);
+    }
+
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
+
+        this.setState(() => ({ error }));
+
+        if (!error) {
+            e.target.elements.option.value = "";
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form id="addOptionForm" onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" placeholder="put in an option" />
+                    <button type="submit" name="submitButton">
+                        Add Option
+          </button>
+                </form>
+            </div>
+        )
+    }
+}
