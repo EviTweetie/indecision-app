f5aef9ae953b32c140035f9b39d8b8236d86d594
EviTweetie <EviTweetie@gmail.com>
2018-06-24 11:36:20 +0200

    7.61 use shorthand in functional component

---
+++ src/components/OptionModal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Modal from 'react-modal'
+
+/* const OptionModal = () => {
+    return (
+        <div>some text</div>
+    )
+} */
+
+//shorthand
+const OptionModal = () => (
+  <div>some text</div>
+)
+
+export default OptionModal
