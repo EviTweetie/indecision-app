47049600a48e08a9a3dee7743db659a74773bc7f
EviTweetie <EviTweetie@gmail.com>
2018-06-24 02:32:10 +0200

    7.60 render props.children V1

--- src/app.js
+++ src/app.js
@@ -2,4 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from './components/IndecisionApp'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"))
+const Layout = (props) => {
+    return (
+        <div>
+        <p>Header</p>
+        <hr />
+        {props.content}
+        <hr />
+        <p>Footer</p>
+      </div>
+    )
+}
+
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is the content</p>
+    </div>
+)
+
+ReactDOM.render(<Layout content={template}/>, document.getElementById("app"))
